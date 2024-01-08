6e0cb971fbf946386dfa053e17df86940be378f6
Cisco29 <castro.francisco@live.fr>
2024-01-08 10:33:43 +0100

    Premier commit - Description des modifications

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    'dark-primary': '#2A254B',
+    'primary': '#4E4D93',
+    'light-grey': '#F9F9F9',
+    'border-grey':'#EBE8F4',
+    'border-dark': '#CAC6DA',
+    'white': '#FFF',
+    extend: {},
+  },
+  plugins: [],
+}
+
